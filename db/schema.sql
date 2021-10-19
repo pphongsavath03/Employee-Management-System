6c4cdac38a7dd3a86dcd76d24973e322b265f3ba
pphong <pphongsavath03@gmail.com>
2021-10-18 21:19:34 -0400

    db update

--- db/schema.sql
+++ db/schema.sql
@@ -1,26 +1,28 @@
 DROP DATABASE IF EXISTS employees_db;
+
 CREATE DATABASE employees_db;
+
 USE employees_db;
-CREATE TABLE department (
-  id INT AUTO_INCREMENT NOT NULL,
-  department_name VARCHAR(30) NULL,
-  PRIMARY KEY (id)
-);
-CREATE TABLE roles (
-  id INT AUTO_INCREMENT NOT NULL,
-  title VARCHAR(100) NULL,
-  salary INT NULL,
-  department_id INT NULL,
-  PRIMARY KEY (id),
-  FOREIGN KEY (department_id) REFERENCES department(id)
-);
+
 CREATE TABLE employee (
-  id INT AUTO_INCREMENT NOT NULL,
+  id INT NOT NULL AUTO_INCREMENT,
   first_name VARCHAR(30) NULL,
   last_name VARCHAR(30) NULL,
   role_id INT NULL,
   manager_id INT NULL,
-  FOREIGN KEY (role_id) REFERENCES roles(id),
-  FOREIGN KEY (manager_id) REFERENCES employee(id),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NULL,
+  salary DECIMAL(10.3) NULL,
+  department_id INT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30) NULL,
   PRIMARY KEY (id)
 );
